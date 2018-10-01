062cdea4625204a48d6d85f41729559d03989f20
Thanapat Waewsri <thorlivers@users.noreply.github.com>
2018-10-01 12:04:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thorliverst/fragmentapplication/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.thorliverst.fragmentapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    loadFragment(new ProfileFragment());
+                    return true;
+                case R.id.navigation_dashboard:
+                    loadFragment(new ScheduleFragment());
+                    return true;
+                case R.id.navigation_notifications:
+                    loadFragment(new MovieFragment());
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new ProfileFragment());
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+}
